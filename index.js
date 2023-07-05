27f85ee715c7adcb09720f491477aa58102390fe
João Paulo <joaowais@gmail.com>
2023-07-04 23:04:55 -0300

    Adiciona o método depositar a classe ContaCorrente

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ class ContaCorrente {
       console.log('Saldo Insuficiente!');
     }
   }
+
+  depositar(valor) {
+    if (valor > 0) {
+      this.saldo += valor;
+      console.log(`Depósito efetuado com sucesso. Saldo atual: ${this.saldo}`);
+    } else {
+      console.log('Você não pode depositar um valor negativo ou igual a 0.');
+    }
+  }
 }
 
 const cliente1 = new Cliente();
@@ -25,5 +34,13 @@ cliente1.cpf = 11122233345;
 const cliente2 = new Cliente();
 cliente2.nome = 'Julio';
 cliente2.cpf = 22233344456;
+
+
+const contaCorrenteC1 = new ContaCorrente();
+contaCorrenteC1.saldo = 0;
+contaCorrenteC1.agencia = 1001;
+
+contaCorrenteC1.depositar(500);
+contaCorrenteC1.sacar(200);
 
 
