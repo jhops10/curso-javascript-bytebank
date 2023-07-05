372e1cb979afd78680afb681e4063373709234b8
João Paulo <joaowais@gmail.com>
2023-07-05 18:54:26 -0300

    tornaldo o atributo saldo privado utilizando a convenção de _ antes do nome

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ class Cliente {
 
 class ContaCorrente {
   agencia;
-  saldo;
+  _saldo;
 
   sacar(valor) {
-    if (this.saldo >= valor) {
-      this.saldo -= valor;
-      console.log(`O seu novo saldo é de: ${this.saldo} Reais`)
+    if (this._saldo >= valor) {
+      this._saldo -= valor;
+      console.log(`O seu novo saldo é de: ${this._saldo} Reais`)
     } else {
       console.log('Saldo Insuficiente!');
     }
@@ -18,8 +18,8 @@ class ContaCorrente {
 
   depositar(valor) {
     if (valor > 0) {
-      this.saldo += valor;
-      console.log(`Depósito efetuado com sucesso. Saldo atual: ${this.saldo}`);
+      this._saldo += valor;
+      console.log(`Depósito efetuado com sucesso. Saldo atual: ${this._saldo}`);
     } else {
       console.log('Você não pode depositar um valor negativo ou igual a 0.');
     }
@@ -37,7 +37,7 @@ cliente2.cpf = 22233344456;
 
 
 const contaCorrenteC1 = new ContaCorrente();
-contaCorrenteC1.saldo = 0;
+contaCorrenteC1._saldo = 0;
 contaCorrenteC1.agencia = 1001;
 
 contaCorrenteC1.depositar(500);
