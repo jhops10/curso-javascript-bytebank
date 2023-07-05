6b1113f407b8e41f78d4237d70996bb031b744e6
João Paulo <joaowais@gmail.com>
2023-07-05 20:04:05 -0300

    adicinado o metodo transferir

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,11 +1,12 @@
 export default class ContaCorrente {
   agencia;
+  cliente;
+
   _saldo;
 
   sacar(valor) {
     if (this._saldo >= valor) {
       this._saldo -= valor;
-      console.log(`O seu novo saldo é de: ${this._saldo} Reais`)
     } else {
       console.log('Saldo Insuficiente!');
     }
@@ -14,9 +15,11 @@ export default class ContaCorrente {
   depositar(valor) {
     if (valor > 0) {
       this._saldo += valor;
-      console.log(`Depósito efetuado com sucesso. Saldo atual: ${this._saldo}`);
-    } else {
-      console.log('Você não pode depositar um valor negativo ou igual a 0.');
     }
+  }
+
+  transferir(valor, conta) {
+    this.sacar(valor);
+    conta.depositar(valor);
   }
 }
--- index.js
+++ index.js
@@ -6,17 +6,34 @@ const cliente1 = new Cliente();
 cliente1.nome = 'João';
 cliente1.cpf = 11122233345;
 
-
 const cliente2 = new Cliente();
 cliente2.nome = 'Julio';
 cliente2.cpf = 22233344456;
 
 
+
 const contaCorrenteC1 = new ContaCorrente();
 contaCorrenteC1._saldo = 0;
 contaCorrenteC1.agencia = 1001;
+contaCorrenteC1.cliente = cliente1;
+contaCorrenteC1.depositar(100);
 
-contaCorrenteC1.depositar(500);
-contaCorrenteC1.sacar(200);
+
+const contaCorrenteC2 = new ContaCorrente();
+contaCorrenteC2._saldo = 0;
+contaCorrenteC2.agencia = 1001;
+contaCorrenteC2.cliente = cliente2;
+
+
+//Ações
+contaCorrenteC1.transferir(25, contaCorrenteC2);
+contaCorrenteC2.transferir(5, contaCorrenteC1);
+
+
+console.log(contaCorrenteC1);
+console.log(contaCorrenteC2);
+
+
+
 
 
