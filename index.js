2cb76b80bcc75f537664db7c5eb440c4def0dab5
João Paulo <joaowais@gmail.com>
2023-07-05 22:08:07 -0300

    criando getters e setters em ContaCorrente para melhor encapsulamento da classe

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const contaCorrenteC1 = new ContaCorrente();
 contaCorrenteC1._saldo = 0;
 contaCorrenteC1.agencia = 1001;
 contaCorrenteC1.cliente = cliente1;
-contaCorrenteC1.depositar(100);
+
 
 
 const contaCorrenteC2 = new ContaCorrente();
@@ -25,13 +25,12 @@ contaCorrenteC2.agencia = 1001;
 contaCorrenteC2.cliente = cliente2;
 
 
+
 //Ações
+contaCorrenteC1.depositar(100);
 contaCorrenteC1.transferir(25, contaCorrenteC2);
 contaCorrenteC2.transferir(5, contaCorrenteC1);
 
-
-console.log(contaCorrenteC1);
-console.log(contaCorrenteC2);
 
 
 
