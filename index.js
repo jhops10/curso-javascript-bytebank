3a8a18c58d3155268bf2e79cef0abba79d283e2c
João Paulo <joaowais@gmail.com>
2023-07-10 21:31:30 -0300

    adicionado classe Funcionario, Gerente, Diretor

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import Cliente from './Cliente.js';
-import ContaCorrente from './ContaCorrente.js';
-import ContaPoupanca from './ContaPoupanca.js';
+import ContaCorrente from './Conta/ContaCorrente.js';
+import ContaPoupanca from './Conta/ContaPoupanca.js';
+import ContaSalario from './Conta/ContaSalario.js';
 
 
 const cliente1 = new Cliente('João', 11122233345);
 
 const contaCorrenteC1 = new ContaCorrente(1001, cliente1);
 const contaPoupancaC1 = new ContaPoupanca(0, cliente1, 1001);
+const contaSalarioC1 = new ContaSalario(cliente1);
 
 //Ações
 
@@ -18,6 +20,7 @@ contaPoupancaC1.sacar(100);
 
 console.log(contaCorrenteC1);
 console.log(contaPoupancaC1);
+console.log(contaSalarioC1);
 
 
 
