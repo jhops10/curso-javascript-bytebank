1c8c72dda036b425a38056e85a85dc63944be24c
João Paulo <joaowais@gmail.com>
2023-07-11 16:27:31 -0300

    adicionado SistemaAutenticacao e senha aos funcionarios

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import Cliente from './Cliente.js';
+import Diretor from './Funcionarios/Diretor.js';
+import Gerente from './Funcionarios/Gerente.js';
 import ContaCorrente from './Conta/ContaCorrente.js';
 import ContaPoupanca from './Conta/ContaPoupanca.js';
 import ContaSalario from './Conta/ContaSalario.js';
+import SistemaAutenticacao from './SistemaAutenticacao.js'
 
 
 const cliente1 = new Cliente('João', 11122233345);
@@ -10,17 +13,15 @@ const contaCorrenteC1 = new ContaCorrente(1001, cliente1);
 const contaPoupancaC1 = new ContaPoupanca(0, cliente1, 1001);
 const contaSalarioC1 = new ContaSalario(cliente1);
 
+const diretor = new Diretor('Ronaldo', 10000, 12332145631);
+diretor.cadastrarSenha('123456789');
+const gerente = new Gerente('Astolfo', 5000, 87662364531);
+gerente.cadastrarSenha('123');
+
+
+
 //Ações
 
-contaCorrenteC1.depositar(500);
-contaCorrenteC1.sacar(100);
+const estaLogado = SistemaAutenticacao.login(diretor, "123456789");
 
-contaPoupancaC1.depositar(500);
-contaPoupancaC1.sacar(100);
-
-console.log(contaCorrenteC1);
-console.log(contaPoupancaC1);
-console.log(contaSalarioC1);
-
-
-
+console.log(estaLogado);
